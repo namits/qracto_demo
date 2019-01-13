f0c40832e51353a4e1ebe8138a4355fd1e0510f4
Namit <namitsrivastava93@gmail.com>
2019-01-13 22:48:07 +0530

    application.rb modification

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,18 @@ module DevdapApp
 
     config.assets.enabled = true
 
-    config.assets.paths << Rails.root.join('vendor', 'app', 'assets', 'fonts')
+
+    config_files = ['secrets.yml']
+
+    config_files.each do |file_name|
+      file_path = File.join(Rails.root, 'config', file_name)
+      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+      config_keys.each do |k,v|
+        ENV[k.upcase] ||= v
+      end
+    end
+
+    config.assets.paths << Rails.root.join('vendor', 'app', 'assets')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
