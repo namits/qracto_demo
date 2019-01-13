3112af4f6de85cb4d1b70cfb85154509546c4457
Namit <namitsrivastava93@gmail.com>
2019-01-13 22:38:23 +0530

    Application fixes

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module DevdapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.encoding = "utf-8"
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir["#{config.root}/lib/modules/**"]
+
+    config.assets.enabled = true
+
     config.assets.paths << Rails.root.join('vendor', 'app', 'assets', 'fonts')
 
     # Settings in config/environments/* take precedence over those specified here.
