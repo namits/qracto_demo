c11c9efe7d01e28316e6a23619c364cf7abacbe5
Namit <namitsrivastava93@gmail.com>
2019-01-13 21:59:47 +0530

    Application.rb fixes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DevdapApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join('vendor')
+    config.secret_key_base = '0978a8db58ff16c1500f24afd4f15430ae52181d5d816186a849b7403ef7a15f673cbc21da475212d617ad907a75f6338aa26da0445b97cbffedb7699a2e6b9a'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
