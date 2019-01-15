16ed08b79d25652c11e7577aaefdbe7d734800f7
Namit <namitsrivastava93@gmail.com>
2019-01-15 23:14:23 +0530

    Production.rb finalysing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,32 +4,21 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = true
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
 
-  # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
+  config.read_encrypted_secrets = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
