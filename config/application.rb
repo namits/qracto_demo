51e47f6de0f62f792a5964908a0e288186f29125
Namit <namitsrivastava93@gmail.com>
2019-01-15 23:32:25 +0530

    Revert "Assets Pipeline added"
    This reverts commit 1e67006ba5aae2a4bbc1dff5e44d084e4ead18d1.
    
    # Conflicts:
    #	config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,6 @@ module DevdapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.encoding = "utf-8"
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.autoload_paths += Dir["#{config.root}/lib/modules/**"]
-
-    config.assets.enabled = true
-
 
     config_files = ['secrets.yml']
 
@@ -29,9 +22,5 @@ module DevdapApp
     end
 
     config.assets.paths << Rails.root.join('vendor', 'app', 'assets')
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
